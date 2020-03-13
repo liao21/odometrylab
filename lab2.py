84f56e08e209e98102e1d5d94e1a38c643cd5815
liao21 <liao21@up.edu>
2020-03-13 11:44:15 -0700

    Robot runs, sensors work, doesn't do conversions
    Robot runs, prints encoding numbers, follow a black tape line, doesn't convert encoder units to anything.

--- lab2.py
+++ lab2.py
@@ -14,9 +14,8 @@ import enum
 socketLock = threading.Lock()
 
 # You should fill this in with your states
-
-
 class States(enum.Enum):
+    LISTEN = enum.auto()
     ON_LINE = enum.auto()
     CORRECTING_RIGHT = enum.auto()
     CORRECTING_LEFT = enum.auto()
@@ -29,18 +28,15 @@ class StateMachine():
         # CONFIGURATION PARAMETERS
         self.IP_ADDRESS = "192.168.1.102" 	# SET THIS TO THE RASPBERRY PI's IP ADDRESS
         self.CONTROLLER_PORT = 5001
-        # If its unable to connect after 10 seconds, give up.  Want this to be a while so robot can init.
-        self.TIMEOUT = 10
-        self.STATE = States.ON_LINE        # Our initial state is LISTEN
-        # We are running if our state machine is initialized
-        self.RUNNING = True
+        self.TIMEOUT = 10					# If its unable to connect after 10 seconds, give up.  Want this to be a while so robot can init.
+        self.STATE = States.LISTEN          # Our initial state is LISTEN
+        self.RUNNING = True                 # We are running if our state machine is initialized
         self.DIST = False
 
         # connect to the motorcontroller
         try:
             with socketLock:
-                self.sock = socket.create_connection(
-                    (self.IP_ADDRESS, self.CONTROLLER_PORT), self.TIMEOUT)
+                self.sock = socket.create_connection( (self.IP_ADDRESS, self.CONTROLLER_PORT), self.TIMEOUT)
                 self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
             print("Connected to RP")
         except Exception as e:
@@ -48,8 +44,7 @@ class StateMachine():
             sys.exit(0)
 
         # Collect events until released
-        self.listener = keyboard.Listener(
-            on_press=self.on_press, on_release=self.on_release)
+        self.listener = keyboard.Listener(on_press=self.on_press, on_release=self.on_release)
         self.listener.start()
 
     def main(self):
@@ -70,6 +65,10 @@ class StateMachine():
         # BEGINNING OF THE CONTROL LOOP
         while(self.RUNNING):
             sleep(0.05)
+            if self.STATE == States.LISTEN:
+                print("STATE: LISTEN")
+                # pass
+                self.STATE = States.ON_LINE
             if self.STATE == States.ON_LINE:
                 # print("Left Sensor: {0}\t\tRight Sensor: {1}".format(self.sensors.left_sensor, self.sensors.right_sensor))
                 with socketLock:
@@ -96,7 +95,7 @@ class StateMachine():
                 # # then turn off and close connection
                 # self.sock.sendall("c".  encode())
                 # print(self.sock.recv(128).decode())
-
+                
                 # self.sock.close()
                 self.STATE = States.ON_LINE
 
@@ -146,8 +145,7 @@ class StateMachine():
         if key == keyboard.Key.esc or key == keyboard.Key.ctrl:
             # Stop listener
             self.RUNNING = False
-            print(
-                "################## [ESC] -- STOP EVERYTHING NOW! ##################")
+            print("################## [ESC] -- STOP EVERYTHING NOW! ##################")
             return False
 
 # END OF STATEMACHINE
@@ -155,60 +153,38 @@ class StateMachine():
 
 class Sensing(threading.Thread):
     def __init__(self, socket):
-        # MUST call this to make sure we setup the thread correctly
-        threading.Thread.__init__(self)
+        threading.Thread.__init__(self)   # MUST call this to make sure we setup the thread correctly
         self.RUNNING = True
         self.sock = socket
         self.left_sensor = 0
         self.right_sensor = 0
-        
-        # self.left_encoder_init = 0
-        # self.right_encoder_init = 0
         self.left_encoder = 0
         self.right_encoder = 0
 
     def run(self):
-        # init = True
         while self.RUNNING:
-            sleep(0.1)
+            sleep(0.2)
             # Sense
-            with socketLock:  # !IMPORTANT: Threadsafe-ness
-                #save bumper sensors to self.left/right_sensor
+            with socketLock: # !IMPORTANT: Threadsafe-ness
                 self.sock.sendall("a cliff_front_left_signal".encode())
                 self.left_sensor = int(self.sock.recv(128).decode())
                 self.sock.sendall("a cliff_front_right_signal".encode())
                 self.right_sensor = int(self.sock.recv(128).decode())
-
-                #save initial encoder values
-                # if init==True:
-                #     self.sock.sendall("a left_encoder_counts".encode())
-                #     self.left_encoder_init = int(self.sock.recv(128).decode())
-                #     self.sock.sendall("a right_encoder_counts".encode())
-                #     self.right_encoder_init = int(self.sock.recv(128).decode())
-
-                #     #left and right encoders are 0
-                #     self.left_encoder = 0
-                #     self.right_encoder = 0
-                #     init = False
-                # else: 
-                    #save normalized encoder values to self.left_encoder and self.right_encoder
                 self.sock.sendall("a left_encoder_counts".encode())
                 self.left_encoder = int(self.sock.recv(128).decode())
                 self.sock.sendall("a right_encoder_counts".encode())
-                self.right_encoder = int(self.sock.recv(128).decode())
-                    # self.left_encoder = self.left_encoder - self.left_encoder_init
-                    # self.right_encoder = self.right_encoder - self.right_encoder_init
+                self.right_encoder = int(self.sock.recv(128).decode())              
 
-
-            print("Cliff Front --\tLeft: ", self.left_sensor,
-                  "\t|\tRight: ", self.right_sensor)
-            print("Encoders --\tLeft: ", self.left_encoder,
-                        "\t|\tRight: ", self.right_encoder)
+            print("Cliff Front --\tLeft: ", self.left_sensor, "\t|\tRight: ", self.right_sensor)
+            print("Encoders --\tLeft: ", self.left_encoder, "\t|\ttRight: ", self.right_encoder)
             # Delay to be sure
             # sleep(0.01);
 
-# END OF SENSING
+            # with socketLock: # !IMPORTANT: Threadsafe-ness
+            #     self.sock.sendall("a light_bump_front_left_signal".encode())
+            #     self.left_sensor = int(self.sock.recv(128).decode())
 
+# END OF SENSING
 
 if __name__ == "__main__":
     sm = StateMachine()
